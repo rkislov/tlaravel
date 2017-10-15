8c1e46dba87c54a09812b0a4f1bc3cd67480ece2
Roman <roman@kislovs.ru>
2017-10-15 22:12:28 +0500

    Router simple routs

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,28 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/page', function () {
-    return view('page');
+Route::get('/page', function() {
+
+    echo '<pre>';
+    //print_r($_ENV);
+    //echo config('app.locale');
+    //echo Config::set('app.locale','ru');
+    //echo Config::get('app.locale');
+    //echo env('APP_ENV');
+    echo '</pre>';
+});
+
+
+//Route::post('/comments',function(){
+//    print_r($_POST);
+//});
+//Route::match(['get','post'],'/comments',function(){
+//    print_r($_POST);
+//});
+
+//Route::any('/comments', function (){
+//    print_r($_POST);
+//});
+Route::post('/comments', function (){
+    print_r($_POST);
 });
