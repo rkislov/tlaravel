17b73c29d8c1f5552030f6063bb3a2a33090a035
Roman <roman@kislovs.ru>
2017-10-18 23:17:22 +0500

    db seed with uuid updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-
+use Ramsey\Uuid\Uuid;
 class CreateUsersTable extends Migration
 {
     /**
@@ -14,7 +14,8 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->uuid('id')->unique();
+            $table->primary('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
